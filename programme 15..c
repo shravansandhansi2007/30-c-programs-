ab64d6f18aee884fc2b5ad0e0f12ef74bf02da31
shravansandhansi2007 <shravanshimpi2007@gmail.com>
2026-09-17 23:07:47 +0530

    Add files via upload

---
+++ programme 15..c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main()
+{
+    int a;
+    float area;
+     printf("Enter the length of square:");
+    scanf("%d", &a);
+    area =a*a;
+    printf("area of square is %2f", area);
+
+  return 0;
+}
---
+++ programme 16.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+int main()
+{
+    int a=15;
+    int b=5;
+
+
+
+    printf("Product of a and b is %d", a*b);
+
+    return 0;
+}
